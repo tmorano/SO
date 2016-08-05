222bf28c4baee9c1995c9d7e358d1352ef437c47
Tiago Morano <tmorano@cpqi.com>
2016-08-05 19:57:20 -0300

    Initial commit

---
+++ app/controllers/indexController.js
@@ -0,0 +1,7 @@
+var indexController = function($scope, $http) {
+
+    $scope.algorithmOptions = ['Test1', 'Test2', 'Test3'];
+
+};
+
+app.controller("indexController", ["$scope", indexController]);
