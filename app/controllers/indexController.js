5352eb822e95faab8309782d77296b0164055dda
Tiago Morano <tmorano@cpqi.com>
2016-09-12 20:21:47 -0300

    Adicionando configuracoes iniciais para entrada de dados

--- app/controllers/indexController.js
+++ app/controllers/indexController.js
@@ -1,6 +1,43 @@
-var indexController = function($scope, $http) {
+var indexController = function($scope) {
 
-    $scope.algorithmOptions = ['Test1', 'Test2', 'Test3'];
+    initScopes();
+
+    function initScopes(){
+        $scope.algorithmOptions = ['Round Robin', 'Least Time to Go (LTG)', 'Merge Fit'];
+        $scope.algorithmSelected = 'Round Robin';
+        $scope.processStates = ['Pronto', 'Esperando', 'Executando'];
+        $scope.cores = 0;
+        $scope.numberOfProcess = 0;
+        $scope.quantum = 0;
+        $scope.processArray = [];
+    }
+
+    $scope.createProcess = function (){
+        var eachProcess = {
+            id : $scope.numberOfProcess++,
+            executionTime : getRandomInt(4,20),
+            processState : $scope.processStates.find('Pronto'),
+            priority : getRandomInt(0,3),
+            interval : 'TODO'
+        };
+        if(algorithmSelected == algorithmOptions.find('Least Time to Go (LTG)')){
+            eachProcess.deadline = 'TODO';
+        }
+
+    };
+
+    $scope.iniciar = function(){
+        if($scope.cores < 1 || $scope.cores > 64){
+            toastr["error"]("Numero de processadores fora da faixa aceitavel");
+            return
+        }
+
+    }
+
+
+    function getRandomInt(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
 
 };
 
