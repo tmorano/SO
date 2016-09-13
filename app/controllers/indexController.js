3c5bcb520d2724c3a65198c7453559c34dc25d0f
Tiago Morano <tiagomorano@gmail.com>
2016-09-13 18:22:28 -0300

    Adicionando/Removendo processos e cores

--- app/controllers/indexController.js
+++ app/controllers/indexController.js
@@ -2,38 +2,77 @@ var indexController = function($scope) {
 
     initScopes();
 
-    function initScopes(){
+    function initScopes() {
         $scope.algorithmOptions = ['Round Robin', 'Least Time to Go (LTG)', 'Merge Fit'];
         $scope.algorithmSelected = 'Round Robin';
         $scope.processStates = ['Pronto', 'Esperando', 'Executando'];
         $scope.cores = 0;
+        $scope.counter = 0;
+        $scope.coresArray = [];
         $scope.numberOfProcess = 0;
         $scope.quantum = 0;
         $scope.processArray = [];
     }
 
-    $scope.createProcess = function (){
-        var eachProcess = {
-            id : $scope.numberOfProcess++,
-            executionTime : getRandomInt(4,20),
-            processState : $scope.processStates.find('Pronto'),
-            priority : getRandomInt(0,3),
-            interval : 'TODO'
-        };
-        if(algorithmSelected == algorithmOptions.find('Least Time to Go (LTG)')){
-            eachProcess.deadline = 'TODO';
+    $scope.checkCores = function () {
+        while ($scope.cores != $scope.coresArray.length) {
+            if ($scope.cores > $scope.coresArray.length) {
+                //Add Cores
+                while ($scope.cores != $scope.coresArray.length) {
+                    var id = $scope.counter;
+                    var eachCore = {
+                        id: id
+                    };
+                    $scope.counter++;
+                    $scope.coresArray.push(eachCore);
+                }
+            } else {
+                //Remove Cores
+                while ($scope.cores != $scope.coresArray.length) {
+                    $scope.coresArray.pop();
+                }
+            }
         }
-
     };
 
-    $scope.iniciar = function(){
-        if($scope.cores < 1 || $scope.cores > 64){
-            toastr["error"]("Numero de processadores fora da faixa aceitavel");
-            return
+    $scope.checkProcess = function () {
+        while($scope.numberOfProcess != $scope.processArray.length){
+            if($scope.numberOfProcess > $scope.processArray.length){
+                //Add process
+                while($scope.numberOfProcess != $scope.processArray.length){
+                    var newProcess = $scope.createProcess();
+                    $scope.processArray.push(newProcess);
+                }
+            }else{
+                // Remove Process
+                while ($scope.numberOfProcess != $scope.processArray.length){
+                    $scope.processArray.pop();
+                }
+            }
         }
+    };
 
-    }
+    $scope.createProcess = function () {
+        var id = $scope.counter;
+        var eachProcess = {
+            id: id,
+            executionTime: getRandomInt(4, 20),
+            processState: 'Pronto',
+            priority: getRandomInt(0, 3),
+            interval: 'TODO'
+        };
+        if ($scope.algorithmSelected == 'Least Time to Go (LTG)') {
+            eachProcess.deadline = 'TODO';
+        }
+        $scope.counter++;
+        return eachProcess;
+    };
 
+    $scope.iniciar = function () {
+        if ($scope.cores < 1 || $scope.cores > 64) {
+            return toastr["error"]("Numero de processadores fora da faixa aceitavel");
+        }
+    };
 
     function getRandomInt(min, max) {
         return Math.floor(Math.random() * (max - min + 1)) + min;
