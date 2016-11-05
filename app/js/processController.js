c8caf6ccdb45691f7eff7e975d739ef640c0610b
Elano Rolim <Elano Rolim>
2016-11-05 02:46:04 -0300

    alterado algoritmo bestfit

--- app/js/processController.js
+++ app/js/processController.js
@@ -3,7 +3,7 @@ sistemasOperacionais.controller('processController', function ($rootScope, $scop
     var memoryService;
 
     $scope.processos = [];
-    $scope.filaDePrioridade = [[], [], [], []]
+    $scope.filaDePrioridade = [[], [], [], []];
     $scope.config;
 
 
@@ -11,10 +11,62 @@ sistemasOperacionais.controller('processController', function ($rootScope, $scop
         $scope.config = args;
 
         //Associa objeto da fabrica para algoritmo especifico
+        // processos = [{
+        //   pid: 0,
+        //   processo: "Processo " + 0,
+        //   progress: 0,
+        //   state: 'Pronto',
+        //   prioridade: 0,
+        //   tempoExecutado: 0,
+        //   tempoTotal: 4,
+        //   memory : 64,
+        //   chance: false
+        // },
+        // {
+        //   pid: 1,
+        //   processo: "Processo " + 1,
+        //   progress: 0,
+        //   state: 'Pronto',
+        //   prioridade: 0,
+        //   tempoExecutado: 0,
+        //   tempoTotal: 12,
+        //   memory : 16,
+        //   chance: false
+        // },
+        // {
+        //   pid: 2,
+        //   processo: "Processo " + 2,
+        //   progress: 0,
+        //   state: 'Pronto',
+        //   prioridade: 0,
+        //   tempoExecutado: 0,
+        //   tempoTotal: 8,
+        //   memory : 32,
+        //   chance: false
+        // },
+        // {
+        //   pid: 3,
+        //   processo: "Processo " + 3,
+        //   progress: 0,
+        //   state: 'Pronto',
+        //   prioridade: 0,
+        //   tempoExecutado: 0,
+        //   tempoTotal: 12,
+        //   memory : 64,
+        //   chance: false
+        // },]
+        // $scope.processos = processos;
+        //
+        // for(var i = 0;i < $scope.processos.length;i++){
+        //   $scope.filaDePrioridade[0].push($scope.processos[i])
+        // }
+
         service = AlgorithmFactoryService.buildAlgorithm($scope.config.algoritmo);
         memoryService = MemoryAlgorithmFactoryService.buildAlgorithm($scope.config.memoryAlgoritmo);
         service.configurar(args);
+        // service.filaDePrioridade = $scope.filaDePrioridade;
         memoryService.iniciarMemoria(args);
+
         createProcess(service, memoryService, args.processos);
         $scope.filaDePrioridade = service.filaDePrioridade;
 
