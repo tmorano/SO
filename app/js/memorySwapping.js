6d929f5ad2b83853c9a663cc5bec53b14992cf4d
Elano Rolim <Elano Rolim>
2016-12-06 17:52:26 -0300

    .

--- app/js/memorySwapping.js
+++ app/js/memorySwapping.js
@@ -10,9 +10,8 @@ var virtualMemory = {};
         var memoryUsage =  function(){
           return Math.floor(((memoryService.memory.totalSize - (newMemory ? (memoryService.memory.size - newMemory) : memoryService.memory.size)) / memoryService.memory.totalSize) * 100);;
         }
-        var blocksRemoved = [];
-
-        if(memoryUsage() > 70){
+        var stop = false;
+        if((usage = memoryUsage()) > 70){
             // Realizar o swap dos processos aguardando, iniciar pelo ultimo da fila de prioridades
             //Percorrer todas as filas de prioridade do round robin
 
@@ -31,8 +30,7 @@ var virtualMemory = {};
                 });
 
                 for(var i = 0;i < processBlocks.length;i++){
-                  if(memoryUsage() > 70){
-                    processo.blocks.splice(i,1);
+                  if((usage = memoryUsage()) > 70 && !processBlocks[i].isVirtual){
                     var block = processBlocks.splice(i,1)[0];
                     var newBlock = angular.copy(block);
                     block.processo = null;
@@ -45,89 +43,84 @@ var virtualMemory = {};
                     newBlock.data = newBlock.data.reverse();
                     delete newBlock.proximo;
 
-                    blocksRemoved.push(newBlock);
                     memoryService.config.memory.size += block.size;
+                    memoryService.encerrarProcesso(null,true);
+                    virtualMemory.allocate(processo,newBlock,memoryService);
                   }else{
+                    stop = true;
                     break;
                   }
                 }
-
-                if(blocksRemoved.length > 0){
-                  memoryService.encerrarProcesso(null,true);
-                  for(var i = 0;i < blocksRemoved.length;i++){
-                    virtualMemory.blocks.push(blocksRemoved[i]);
-                    memoryService.config.arrayOfProcessMemory.series.push(blocksRemoved[i]);
-                  }
-                  blocksRemoved = [];
-                };
+                return !stop;
               });
             });
-
-            // memoryService.config.filaDePrioridade.every(function(eachFila){
-            //     memoryUsage =  Math.floor(((memoryService.memory.totalSize - memoryService.memory.size) / memoryService.memory.totalSize) * 100);
-            //     if(memoryUsage < 70){
-            //         return true;
-            //     }else{
-            //         if(eachFila.length == 0){
-            //             return false;
-            //         }
-            //         for(var j = eachFila.length-1; j>0; j--){
-            //             var proc = eachFila[j];
-            //             if(proc.state == 'Aguardando'){
-            //                 memoryUsage =  Math.floor(((memoryService.memory.totalSize - memoryService.memory.size) / memoryService.memory.totalSize) * 100);
-            //                 if(memoryUsage > 70){
-            //                     //Encontrar processos no bloco do algoritmo
-            //                     memoryService.memory.blocks.forEach(function(eachBlock){
-            //                         if(eachBlock.processo){
-            //                             if(eachBlock.processo.pid == proc.pid){
-            //                                 processToRemove = eachBlock.processo;
-            //                                 processToRemove.isSwapped = true;
-            //                                 MemoryHelper.encerrarProcesso(eachBlock.processo,memoryService);
-            //                             }
-            //                         }
-            //                     });
-            //                     virtualMemory.blocks = [];
-            //                     /**Com o processo encontrado vamos adicionar no HD utilizando o bestfit **/
-            //                     /** ordena para conseguir o melhor bloco **/
-            //                     sortedVirtualBlocks.sort(function(blockA,blockB){ return blockB.size - blockA.size; });
-            //                     var bestFitBlock;
-            //                     for(var i = 0;i < sortedVirtualBlocks.length; i++){
-            //                         if(!sortedVirtualBlocks[i].processo && sortedVirtualBlocks[i].size >= processToRemove.size){
-            //                             bestFitBlock = sortedVirtualBlocks[i];
-            //                         }
-            //                     }
-            //                     /** não encontrou um bloco **/
-            //                     if(!bestFitBlock){
-            //
-            //                         var block = {
-            //                             id: virtualMemory.blocks.length,
-            //                             processo: processToRemove,
-            //                             size: processToRemove.memory,
-            //                             data: [0,processToRemove.memory],
-            //                             name: 'Processo ' + processToRemove.pid,
-            //                             usado: processToRemove.memory,
-            //                         };
-            //                         /** incrementa a quantidade total de blocos criados **/
-            //                         virtualMemory.blocks.push(block);
-            //                         bestFitBlock = block;
-            //                         /** lista auxiliar de blocos ordenados pelo tamanho **/
-            //                         sortedVirtualBlocks.push(block);
-            //                         memoryService.config.arrayOfProcessMemory.series.push(block);
-            //                     }else{
-            //                         /** se encontrou um bloco aloca ele **/
-            //                         bestFitBlock.name = 'Processo ' + processToRemove.pid;
-            //                         bestFitBlock.processo = processToRemove;
-            //                         bestFitBlock.usado = processToRemove.memory < bestFitBlock.size ? processToRemove.memory : bestFitBlock.size;
-            //                     }
-            //                 }
-            //             }
-            //         }
-            //         return false;
-            //     }
-            // });
         }else{
-          
+          if(virtualMemory.blocks.length == 0) return;
+          memoryService.config.filaDePrioridade.forEach(function(fila){
+            fila.filter(function(processo){
+              return processo.state == 'Abortado';
+            })
+            .every(function(processo){
+              var processBlocks = [];
+              processo.blocks.forEach(function(id){
+                processBlocks = processBlocks.concat(virtualMemory.blocks.filter(function(block){
+                  return block.id == id;
+                }));
+              });
+
+              if(memoryService.split){
+                for(var i = 0;i < processBlocks.length; i++){
+                  if((usage = memoryUsage()) <= 70){
+                    memoryService.split(processo,processBlocks[i].size,memoryService.memory.blocks[0],0);
+                    processBlocks[i].name = 'DISPONIVEL';
+                    processBlocks[i].usado = 0;
+                    processBlocks[i].processo = null;
+                  }else{
+                    stop = true;
+                    break;
+                  }
+                }
+              }
+              return !stop;
+            })
+          });
         }
     }
+
+    virtualMemory.allocate = function(processo,block,memoryService){
+      /**Com o processo encontrado vamos adicionar no HD utilizando o bestfit **/
+      /** ordena para conseguir o melhor bloco **/
+      sortedVirtualBlocks.sort(function(blockA,blockB){ return blockB.size - blockA.size; });
+      var bestFitBlock;
+      for(var i = 0;i < sortedVirtualBlocks.length; i++){
+        if(!sortedVirtualBlocks[i].processo && sortedVirtualBlocks[i].size >= processToRemove.size){
+          bestFitBlock = sortedVirtualBlocks[i];
+        }
+      }
+      /** não encontrou um bloco **/
+      if(!bestFitBlock){
+
+        // var block = {
+        //   id: virtualMemory.blocks.length,
+        //   processo: processToRemove,
+        //   size: processToRemove.memory,
+        //   data: [0,processToRemove.memory],
+        //   name: 'Processo ' + processToRemove.pid,
+        //   usado: processToRemove.memory,
+        // };
+        /** incrementa a quantidade total de blocos criados **/
+        virtualMemory.blocks.push(block);
+        bestFitBlock = block;
+        /** lista auxiliar de blocos ordenados pelo tamanho **/
+        sortedVirtualBlocks.push(block);
+        memoryService.config.arrayOfProcessMemory.series.push(block);
+      }else{
+        /** se encontrou um bloco aloca ele **/
+        bestFitBlock.name = 'Processo ' + processToRemove.pid;
+        bestFitBlock.processo = processToRemove;
+        bestFitBlock.usado = processToRemove.memory < bestFitBlock.size ? processToRemove.memory : bestFitBlock.size;
+      }
+    }
+
     return virtualMemory;
 });
